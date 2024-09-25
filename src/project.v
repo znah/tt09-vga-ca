c3f13d9ef05c02b83d8f566baa43ee2965651a00
Alexander Mordvintsev <zzznah@gmail.com>
2024-09-25 11:28:18 +0200

    module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_vga_ca(
+module tt_um_znah_vga_ca(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
